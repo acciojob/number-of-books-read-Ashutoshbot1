86da1437d2a23242c6703145cade80cf3d4c687d
Ashutoshbot1 <100514925+Ashutoshbot1@users.noreply.github.com>
2023-06-16 14:33:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let cnt=0;
+
+	for(let book of library){
+		if(book[readingStatus]){
+			cnt++;
+		}
+	}
+
+	return cnt;
 };
 
 // Do not change the code below
